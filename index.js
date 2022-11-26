4ca75d6d9ad98bcf306e34df732ab5e888d8275c
zahidhasanjibone <zahidhasanjibon90@gmail.com>
2022-11-26 16:15:53 +0600

    seller products route added

--- index.js
+++ index.js
@@ -1,180 +1,188 @@
-const express = require("express")
-const cors = require("cors")
-const jwt = require("jsonwebtoken")
-const dotenv = require("dotenv")
+const express = require("express");
+const cors = require("cors");
+const jwt = require("jsonwebtoken");
+const dotenv = require("dotenv");
 
+const app = express();
 
-const app = express()
+app.use(cors());
+dotenv.config();
+app.use(express.json());
 
+const PORT = process.env.PORT || 5000;
 
-app.use(cors())
-dotenv.config()
-app.use(express.json())
+// mongodb connection
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const uri = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@cluster0.nxpijbg.mongodb.net/?retryWrites=true&w=majority`;
+client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
 
-const PORT  = process.env.PORT || 5000
+// internal import
+app.get("/", (req, res) => {
+  res.send({ message: "server running" });
+});
 
-        // mongodb connection
-        const { MongoClient, ServerApiVersion } = require('mongodb');
-        const uri = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@cluster0.nxpijbg.mongodb.net/?retryWrites=true&w=majority`;
-         client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-   
-    // internal import
-    app.get("/",(req,res) => {
-        res.send({message:"server running"})
-      })
-
-      // get all course rout
+// get all course rout
 async function run() {
-    const usersCollection = client.db("ph-assign-12").collection("users");
-    const productsCollection = client.db("ph-assign-12").collection("products");
-    const bookingCollection = client.db("ph-assign-12").collection("booking");
+  const usersCollection = client.db("ph-assign-12").collection("users");
+  const productsCollection = client.db("ph-assign-12").collection("products");
+  const bookingCollection = client.db("ph-assign-12").collection("booking");
+  const productCategoryCollection = client
+    .db("ph-assign-12")
+    .collection("productCategory");
 
-  
-    try {
-        // create jwt token route
-        app.post("/jwtgenerate", (req, res) => {
-          const userData = req.body;
+  try {
+    // create jwt token route
+    app.post("/jwtgenerate", (req, res) => {
+      const userData = req.body;
 
-          const token = jwt.sign(userData, process.env.JSON_SECRET, {
-            expiresIn: "5d",
-          });
-          res.send({
-            token,
-          });
-        });
+      const token = jwt.sign(userData, process.env.JSON_SECRET, {
+        expiresIn: "5d",
+      });
+      res.send({
+        token,
+      });
+    });
 
-      // save user info in database
+    // save user info in database
 
-      app.post("/register",async (req,res) => {
+    app.post("/register", async (req, res) => {
+      const user = req.body;
+      // check wether user already exist
+      const query = { userEmail: user.userEmail };
+      const alreadySave = await usersCollection.find(query).toArray();
 
-        const user = req.body
-        // check wether user already exist
-        const query = {userEmail:user.userEmail}
-        const alreadySave = await usersCollection.find(query).toArray()
+      if (alreadySave.length) {
+        return res.send({ message: "user already exist" });
+      }
 
-        if(alreadySave.length){
-          return res.send({message:"user already exist"})
-        }
+      const result = await usersCollection.insertOne(user);
+      res.send(result);
+    });
 
-        const result = await usersCollection.insertOne(user)
-          res.send(result)
-      })
-  
-      // get all users
+    // get all users
+
+    // update user to admin or vice versa
+
+    // create payment route
+
+    // create seller product add route
+
+    app.post("/product", async (req, res) => {
+      const product = req.body;
+      let timeWhenPost = new Date()
+      timeWhenPost = timeWhenPost.toString().slice(0,15)
+      product.timeWhenPost = timeWhenPost
+      const result = await productsCollection.insertOne(product);
+      res.send(result);
+    });
+
+    // get all products
+
+    app.get("/products", async (req, res) => {
+      const query = {};
+      const products = await productsCollection.find(query).toArray();
+      res.send(products);
+    });
+
+
+    //  get especific category product
+
+    app.get("/products/:name", async (req, res) => {
+      const categoryName = req.params.name;
+      const query = { category: categoryName };
+      const result = await productsCollection.find(query).toArray();
+      res.send(result);
+    });
+
+    // get seller all products
+
+    app.get("/sellerproducts",async (req,res) => {
+
+      const sellerEmail = req.query.email
+
+      const query = {sellerEmail:sellerEmail}
+
+      const result = await productsCollection.find(query).toArray()
       
-      // update user to admin or vice versa
-  
-      // create payment route
+      res.send(result)
 
-
-
-      // create seller product add route
-
-
-        app.post("/product",async(req,res) => {
-
-            const product = req.body
-            product.timeWhenPost = Date.now()
-            const result = await productsCollection.insertOne(product)
-            res.send(result)
-        })
-
-
-        // get all products
-
-        app.get("/products",async (req,res) => {
-            const query = {}
-            const products = await productsCollection.find(query).toArray()
-            res.send(products)
-        })
-
-
-        //  get especific category product
-
-        app.get("/products/:name",async (req,res) => {
-            const categoryName = req.params.name
-              const query  = {category:categoryName}
-              const result = await productsCollection.find(query).toArray()
-              res.send(result)
-        })
-
-
-        // check user role 
-
-        app.get("/user/checkrole/:email",async (req,res) => {
-
-            const email = req.params.email
-            const query = {userEmail:email}
-            const user = await usersCollection.findOne(query)
-            res.send({role:user?.role})      
-        })
-
-
-        // booking producct route
-
-        app.post("/booking",async (req,res) => {
-
-        const bookingData = req.body
-
-          const result = await bookingCollection.insertOne(bookingData)
-
-          res.send(result)
-
-        })
-
-
-          // get all orders 
-
-          app.get("/orders",async (req,res) => {
-              const {email} = req.query
-            const query = {buyerEmail:email}
-            const result   = await bookingCollection.find(query).toArray()
-
-            res.send(result)
-
-          })
-
-
-            // get all sellers
-
-            app.get("/sellers",async (req,res) => {
-
-              const query = {role:"seller"}
-
-              const result = await usersCollection.find(query).toArray()
-              res.send(result)
-
-            })
-
-            // get all sellers
-
-            app.get("/buyers",async (req,res) => {
-
-              const query = {role:"buyer"}
-
-              const result = await usersCollection.find(query).toArray()
-              res.send(result)
-
-            })
-
-
-
-
-
-
-
-  
-    } finally {
-    }
-  }
-  run().catch((err) => {
-    console.log(err);
-  });
-
-
-
-    app.listen(PORT,() => {
-        console.log(`server listen on port ${PORT}`);
     })
 
 
+    // check user role
+
+    app.get("/user/checkrole/:email", async (req, res) => {
+      const email = req.params.email;
+      const query = { userEmail: email };
+      const user = await usersCollection.findOne(query);
+      res.send({ role: user?.role });
+    });
+
+    // booking producct route
+
+    app.post("/booking", async (req, res) => {
+      const bookingData = req.body;
+
+      const result = await bookingCollection.insertOne(bookingData);
+
+      res.send(result);
+    });
+
+    // get all orders
+
+    app.get("/orders", async (req, res) => {
+      const { email } = req.query;
+      const query = { buyerEmail: email };
+      const result = await bookingCollection.find(query).toArray();
+
+      res.send(result);
+    });
+
+    // get all sellers
+
+    app.get("/sellers", async (req, res) => {
+      const query = { role: "seller" };
+
+      const result = await usersCollection.find(query).toArray();
+      res.send(result);
+    });
+
+    // get all sellers
+
+    app.get("/buyers", async (req, res) => {
+      const query = { role: "buyer" };
+
+      const result = await usersCollection.find(query).toArray();
+      res.send(result);
+    });
+
+    // get product category
+
+    app.get("/productcategory", async (req, res) => {
+      const query = {};
+
+      const result = await productCategoryCollection.find(query).toArray();
+
+      res.send(result);
+    });
+
+
+        // create product route
+
+
+
+
+  } finally {
+  }
+}
+run().catch((err) => {
+  console.log(err);
+});
+
+app.listen(PORT, () => {
+  console.log(`server listen on port ${PORT}`);
+});
